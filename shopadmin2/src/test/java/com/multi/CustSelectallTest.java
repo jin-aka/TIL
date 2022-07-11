0dced51dbb7ef65677f6254098e30fa54ee13e01
jin-aka <kayoungjin@gmail.com>
2022-07-11 09:19:34 +0900

    self study shopadmin2

---
+++ shopadmin2/src/test/java/com/multi/CustSelectallTest.java
@@ -0,0 +1,33 @@
+package com.multi;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.biz.CustBiz;
+import com.multi.vo.CustVO;
+
+@SpringBootTest
+class CustSelectallTest {
+	
+	@Autowired
+	CustBiz biz;
+
+	@Test
+	void contextLoads() {
+		List<CustVO> list = null;
+		try {
+			list = biz.get();
+			for (CustVO custVO : list) {
+				System.out.println(custVO);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
